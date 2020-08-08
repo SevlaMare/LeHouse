d9b1042cf257acc1f232c5175b09d9743e2d7e7b
Mare <sevilamare@gmail.com>
2020-08-08 14:54:10 -0300

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool: "none",
+  module: {
+    rules: [
+      // BABEL
+      {  
+        test: /\.js$/,  
+        exclude: /node_modules/,  
+        use: {  
+          loader: 'babel-loader',  
+          options: { cacheDirectory: true }  
+        }
+      },
+
+      // HTML LOADER + plug
+      {
+        test: /\.html$/,
+        use: [
+          { loader: "html-loader" }
+        ]
+      },
+
+      // FILE LOADER
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/'
+          }
+        }
+      },
+
+      // CSS LOADER + split plug
+      {
+        test: /\.css$/,
+        use:  [ MiniCssExtractPlugin.loader, 'css-loader']
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My app",
+      // favicon: "images/idk.svg",
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css', // css output
+    })
+  ]
+}
