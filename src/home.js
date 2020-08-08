72bd800863ed11a8a3c7f472e38eb82e083c8c7a
Mare <sevilamare@gmail.com>
2020-08-08 14:40:25 -0300

    templates

---
+++ src/home.js
@@ -0,0 +1,11 @@
+const renderHome = (() => {
+  const sel = document.querySelector('#content');
+
+  const title = document.createElement('h1');
+
+  title.innerHTML = 'Le House is the house!';
+
+  sel.append(title);
+})();
+
+export default renderHome;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import renderNav from './navbar';
+import renderHome from './home';
+import css from './css/style.css';
+
+renderNav();
+renderHome();
